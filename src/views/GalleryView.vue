478d49ddc88b2bc71d9a7683584e8ddb708a97fa
Beela303 <devbeela@gmail.com>
2026-01-15 23:51:53 +0100

    updated favican and art

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -10,6 +10,7 @@
             <img src="../assets/images/below the interspace of stars and clouds.jpg"
                 alt="Below the interspace of stars and clouds">
             <img src="../assets/images/below the ocean (2).jpg" alt="Below the ocean">
+            <img src="../assets/images/trad 05.jpg" alt="Traditional Art">
             <img src="../assets/images/brown aesthetic.jpg" alt="Brown aesthetic">
             <img src="../assets/images/calm ambience.jpg" alt="Calm ambience">
             <img src="../assets/images/Fashionista in a museum (blue frame, black bg).jpg"
@@ -18,14 +19,22 @@
                 alt="fashionista in a museum (blue frame, white bg)">
             <img src="../assets/images/fashionista in a museum (pink frame, black bg).jpg"
                 alt="fashionista in a museum (pink frame, black bg)">
-            <img src="../assets/images/fashionista in a museum (pink frame, white bg).jpg"
-                alt="fashionista in a museum (pink frame, white bg)">
-            <img src="../assets/images/Fashionista in a museum (white bg).jpg" alt="Fashionista in a museum (white bg)">
             <img src="../assets/images/galactic ocean of dreams.jpg" alt="Galactic ocean of dreams">
+            <img src="../assets/images/trad 09.jpg" alt="Traditional Art">
             <img src="../assets/images/hall of mirage (border).jpg" alt="Hall of mirage (border)">
             <img src="../assets/images/hall of mirage.jpg" alt="Hall of mirage">
+            <img src="../assets/images/Fashionista in a museum (white bg).jpg" alt="Fashionista in a museum (white bg)">
+            <img src="../assets/images/trad 12.jpg" alt="Traditional Art">
             <img src="../assets/images/the city starry sky (day).jpg" alt="The city's starry sky (day)">
             <img src="../assets/images/the city starry sky (night).jpg" alt="The city's starry sky (night)">
+            <img src="../assets/images/trad 06.jpg" alt="Traditional Art">
+            <img src="../assets/images/fashionista in a museum (pink frame, white bg).jpg"
+                alt="fashionista in a museum (pink frame, white bg)">
+            <img src="../assets/images/trad 07.jpg" alt="Traditional Art">
+            <img src="../assets/images/trad 08.jpg" alt="Traditional Art">
+            <img src="../assets/images/trad 10.jpg" alt="Traditional Art">
+            <img src="../assets/images/trad 11.jpg" alt="Traditional Art">
+            <img src="../assets/images/trad 13.jpg" alt="Traditional Art">
         </div>
     </div>
 </template>
